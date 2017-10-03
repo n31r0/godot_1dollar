16e99aa375308ac296f75cb05ad440c7977ec2a2
Todor Imreorov <blurymind@gmail.com>
2017-10-03 15:47:41 +0100

    many new features
    - add allowed draw area + mouse indication when you are over it
    - add ability to limit ink - to reduce the size of shapes that can be drawn
    - add ability to create collision shapes with the shape of what is drawn, limit how many can be made and tag them in groups
    - add new shapes: V and pigtail + add more data about line data to better recognise the other shapes
    - add a draw method to draw a line and indicator of ink
    - reduce the recording and recognition from 10 to 4 minimum points - in order to improve better recognition of quick swipes (horizontal and vertical lines)

--- main.gd
+++ main.gd
@@ -1,4 +1,6 @@
-extends Node
+extends Node2D
+
+## todo- turn this into an addon for godot, so it is easier to reuse in other projects
 
 var pressed = false
 var guestures = preload("recognizer.gd").new()
@@ -6,45 +8,55 @@ var draw = []
 var recognize = false
 var position = Vector2()
 var gestureJsonFilePath = "res://recordedGestures.json"
+var maxInk = 1
+onready var curInk = maxInk
+var mouseCursorIconPath = "res://pencil.png"
 
 func _ready():
 	set_process(true)
 	set_process_input(true)
 	position = get_node("gui/cube").get_pos()
-	
-	yield(get_tree(), "idle_frame")
 	loadSavedGesturesFromJson(gestureJsonFilePath)
 
-func _process(delta):
-	pass
+func _process(delta): pass
+#	if curInk>0:
+#		curInk -= 0.001
+#		update()
 
 func _input(event):
-	if (event.is_action_pressed("m_btn")):
-		draw = []
-		pressed = event.pressed
-		get_node("gui/cube").set_emitting(true)
-		position.x = get_viewport().get_mouse_pos().x
-		position.y = get_viewport().get_mouse_pos().y
-		get_node("gui/cube").set_pos(position)
-	if (event.is_action_released("m_btn")): ## detect the gesture from memory
-		pressed = event.pressed
-		if (draw.size() > 10):
-			get_node("gui/status").set_text(str(guestures.recognize(draw)))
-			get_node("gui/addGuester/draw").set_text(str("draw: ",draw.size()," uni: ",guestures.Unistrokes.size()))
-		get_node("gui/cube").set_pos(position)
-		get_node("gui/cube").set_emitting(false)
-	if (event.type == InputEvent.MOUSE_MOTION && pressed):
-		position.x = get_viewport().get_mouse_pos().x
-		position.y = get_viewport().get_mouse_pos().y
-		draw.append(position)
-		get_node("gui/cube").set_pos(position)
+	if canDraw:
+		if (event.is_action_pressed("m_btn")):
+			draw = []
+			pressed = event.pressed
+			get_node("gui/cube").set_emitting(true)
+			position.x = get_viewport().get_mouse_pos().x
+			position.y = get_viewport().get_mouse_pos().y
+			get_node("gui/cube").set_pos(position)
+
+		if (event.is_action_released("m_btn")): ## detect the gesture from memory
+			pressed = event.pressed
+			if (draw.size() > 4):
+				get_node("gui/status").set_text(str(guestures.recognize(draw)," ink left:",curInk))
+				get_node("gui/addGuester/draw").set_text(str("draw: ",draw.size()," uni: ",guestures.Unistrokes.size()))
+				get_node("gui/cube").set_pos(position)
+				get_node("gui/cube").set_emitting(false)
+				drawColShapePolygon(draw)
+				curInk = maxInk
+				update()
+			
+		if (event.type == InputEvent.MOUSE_MOTION && pressed):
+			position.x = get_viewport().get_mouse_pos().x
+			position.y = get_viewport().get_mouse_pos().y
+			draw.append(position)
+			get_node("gui/cube").set_pos(position)
+			update()
 
 var savedGestures = []
 var data = {}
 func _on_addGuester_pressed():
-	if (draw.size() > 10):
+	if (draw.size() > 4):
 		var new_guester = preload("unistroke.gd").new(get_node("gui/addGuester/guester_name").get_text(), draw)
-		guestures.Unistrokes.append(new_guester) ## This is where it stores them!- todo add load/save to json
+		guestures.Unistrokes.append(new_guester)
 		## store to array that will be written to the json file
 		savedGestures.append([get_node("gui/addGuester/guester_name").get_text(),var2str(draw)])
 		print("we have ",savedGestures.size()," gestures so far")
@@ -62,7 +74,6 @@ func saveGesturesToJsonFile():
 	file.store_line(data.to_json())
 	file.close()
 	print("saved ",savedGestures.size()," gestures")
-#	print("saving ",data)
 
 var loadedGestures = []
 func loadSavedGesturesFromJson(path):
@@ -90,4 +101,55 @@ func loadSavedGesturesFromJson(path):
 	print ("Loaded ",loadedGestures.size()," gestures!")
 
 
+## can we draw or not - the mouse needs to be inside the area
+var canDraw = false
+func _on_draw_zone_mouse_enter():
+	canDraw = true
+	Input.set_custom_mouse_cursor(load(mouseCursorIconPath),Vector2(30,20))
 
+func _on_draw_zone_mouse_exit():
+	canDraw = false
+	Input.set_custom_mouse_cursor(null)
+
+########### draw a shape ################
+export var lineThickness = 2
+export var lineColor = Color(255, 0, 0,1)
+export var inkHealthBarWidth = 100
+
+func _draw():## draw a line and an ink health bar
+	var lineIndex = 0
+	for line in draw:
+		if lineIndex > 0 and curInk>0: ##draw freehand line
+			curInk -= 0.001 ## run out of ink when drawing
+			draw_line(draw[lineIndex-1], draw[lineIndex], Color(lineColor.r,lineColor.g,lineColor.b,curInk), lineThickness)
+		lineIndex +=1
+	if curInk > 0 and inkHealthBarWidth > 0 :## indicate how much ink is left
+		draw_rect(Rect2(10,10,curInk*inkHealthBarWidth,20),lineColor)
+
+### draw a colision shape from the array ###
+export var createColisions = true
+export var simplifyColShape = 6
+export var maxDrawnColShapes = 3
+func drawColShapePolygon(vector2arr):
+	if curInk > 0 and createColisions:
+		for repeat in range(simplifyColShape):
+			vector2arr = simplifyArr(vector2arr)
+		print ("col shape points:",vector2arr.size())
+		var colShapePol = CollisionPolygon2D.new()
+		colShapePol.set_polygon(vector2arr)
+		colShapePol.add_to_group(str(guestures.recognize(draw))) ##will be useful later on for colisions
+		colShapePol.add_to_group("drawnShapes") ## use to keep track of all
+		add_child(colShapePol)
+	### limit how many maximum drawn shapes can exist ### you can destroy them on collision elsewhere
+	if maxDrawnColShapes > 0:
+		if get_tree().get_nodes_in_group("drawnShapes").size() > maxDrawnColShapes:
+			get_tree().get_nodes_in_group("drawnShapes")[0].queue_free()## remove the oldest
+
+########## helper functions ###############
+func simplifyArr(inputArray):
+	var itemInd = 0
+	for item in inputArray:
+		if itemInd % 2:
+			inputArray.remove(itemInd)
+		itemInd+=1
+	return inputArray
