1e495f4840cc074c6f18c721daa3b97105939145
Todor Imreorov <blurymind@gmail.com>
2017-10-05 10:41:29 +0100

    refactoring, some new shapes
    Did some code clean up and refactoring in order to move closer to turning this into an addon
    - moved particle2d to runtime code, made it an optional effect
    - created recording mode toggle and separated gui nodes from
    - the main node will now emit a signal with the recognised shape #todo-also add to the signal how much ink was left
    - fixed some flawed parts of the code
    Added new shapes: pigtail, carret, v(better recognition) - total 98 shape entries so far

--- main.gd
+++ main.gd
@@ -1,55 +1,81 @@
 extends Node2D
 
 ## todo- turn this into an addon for godot, so it is easier to reuse in other projects
+signal shapeDetected 
 
 var pressed = false
 var guestures = preload("recognizer.gd").new()
 var draw = []
 var recognize = false
 var position = Vector2()
-var gestureJsonFilePath = "res://recordedGestures.json"
+var scriptPath = self.get_script().get_path().get_base_dir()
+onready var gestureJsonFilePath = scriptPath+"recordedGestures.json"
 var maxInk = 1
 onready var curInk = maxInk
-var mouseCursorIconPath = "res://pencil.png"
 
+var mouseCursorIconPath = scriptPath+"pencil.png"
+export var recording = true
+export var particleEffect = true
+
+var particleNode = null 
 func _ready():
 	set_process(true)
 	set_process_input(true)
-	position = get_node("gui/cube").get_pos()
+	if particleEffect:
+		particleNode = Particles2D.new()
+		particleNode.set_name("drawParticle")
+		particleNode.set_pos(Vector2(50,50))
+		particleNode.set_initial_velocity(Vector2(0,0))
+		particleNode.set_use_local_space(false)
+		particleNode.set_amount(64)
+		particleNode.set_lifetime(1)
+		particleNode.set_param(particleNode.PARAM_LINEAR_VELOCITY,0)
+		particleNode.set_param(particleNode.PARAM_GRAVITY_STRENGTH,0)
+		particleNode.set_param(particleNode.PARAM_INITIAL_SIZE,20)
+		position = particleNode.get_pos()
+		add_child(particleNode)
 	loadSavedGesturesFromJson(gestureJsonFilePath)
 
 func _process(delta): pass
-#	if curInk>0:
-#		curInk -= 0.001
-#		update()
 
 func _input(event):
 	if canDraw:
 		if (event.is_action_pressed("m_btn")):
 			draw = []
 			pressed = event.pressed
-			get_node("gui/cube").set_emitting(true)
 			position.x = get_viewport().get_mouse_pos().x
 			position.y = get_viewport().get_mouse_pos().y
-			get_node("gui/cube").set_pos(position)
+			if particleEffect:
+				particleNode.set_emitting(true)
+				particleNode.set_pos(position)
 
 		if (event.is_action_released("m_btn")): ## detect the gesture from memory
 			pressed = event.pressed
 			if (draw.size() > 4):
-				get_node("gui/status").set_text(str(guestures.recognize(draw)," ink left:",curInk))
-				get_node("gui/addGuester/draw").set_text(str("draw: ",draw.size()," uni: ",guestures.Unistrokes.size()))
-				get_node("gui/cube").set_pos(position)
-				get_node("gui/cube").set_emitting(false)
-				drawColShapePolygon(draw)
-				curInk = maxInk
-				update()
-			
-		if (event.type == InputEvent.MOUSE_MOTION && pressed):
+				recogniseDrawnGesture()
+			curInk = maxInk
+
+		if (event.type == InputEvent.MOUSE_MOTION && pressed) and curInk > 0:
 			position.x = get_viewport().get_mouse_pos().x
 			position.y = get_viewport().get_mouse_pos().y
 			draw.append(position)
-			get_node("gui/cube").set_pos(position)
-			update()
+			if particleEffect:
+				particleNode.set_pos(position)
+			if draw.size() % 2:update() ## dont update so often
+		if curInk <= 0:
+			recogniseDrawnGesture()
+
+func recogniseDrawnGesture():
+	get_node("gui/status").set_text(str(guestures.recognize(draw)," ink left:",curInk))
+	get_node("gui/addGuester/draw").set_text(str("draw: ",draw.size()," uni: ",guestures.Unistrokes.size()))
+	if particleEffect: ## particle effect is optional
+		particleNode.set_pos(position)
+		particleNode.set_emitting(false)
+	drawColShapePolygon(draw)
+	curInk = maxInk
+	update()
+	emit_signal("shapeDetected",guestures.recognize(draw))
+	if not recording : draw = []
 
 var savedGestures = []
 var data = {}
@@ -100,14 +126,14 @@ func loadSavedGesturesFromJson(path):
 		guestures.Unistrokes.append(new_guester)
 	print ("Loaded ",loadedGestures.size()," gestures!")
 
-
 ## can we draw or not - the mouse needs to be inside the area
 var canDraw = false
-func _on_draw_zone_mouse_enter():
+func _on_drawZone_mouse_enter():
 	canDraw = true
 	Input.set_custom_mouse_cursor(load(mouseCursorIconPath),Vector2(30,20))
+	print("inside drawzone")
 
-func _on_draw_zone_mouse_exit():
+func _on_drawZone_mouse_exit():
 	canDraw = false
 	Input.set_custom_mouse_cursor(null)
 
@@ -128,28 +154,32 @@ func _draw():## draw a line and an ink health bar
 
 ### draw a colision shape from the array ###
 export var createColisions = true
-export var simplifyColShape = 6
 export var maxDrawnColShapes = 3
 func drawColShapePolygon(vector2arr):
 	if curInk > 0 and createColisions:
-		for repeat in range(simplifyColShape):
-			vector2arr = simplifyArr(vector2arr)
+		vector2arr = simplifyArr(vector2arr,6)
 		print ("col shape points:",vector2arr.size())
 		var colShapePol = CollisionPolygon2D.new()
 		colShapePol.set_polygon(vector2arr)
-		colShapePol.add_to_group(str(guestures.recognize(draw))) ##will be useful later on for colisions
+		colShapePol.add_to_group(str("drawnShape:",guestures.recognize(draw))) ##will be useful later on for colisions
 		colShapePol.add_to_group("drawnShapes") ## use to keep track of all
 		add_child(colShapePol)
 	### limit how many maximum drawn shapes can exist ### you can destroy them on collision elsewhere
-	if maxDrawnColShapes > 0:
+	if maxDrawnColShapes > 0:##if set to 0, it is disabled
 		if get_tree().get_nodes_in_group("drawnShapes").size() > maxDrawnColShapes:
 			get_tree().get_nodes_in_group("drawnShapes")[0].queue_free()## remove the oldest
 
 ########## helper functions ###############
-func simplifyArr(inputArray):
-	var itemInd = 0
-	for item in inputArray:
-		if itemInd % 2:
-			inputArray.remove(itemInd)
-		itemInd+=1
+func simplifyArr(inputArray,simplifyAmount): ##reduce the number of items in an array by taking out the inbetween ones
+	for repeat in range(simplifyAmount):
+		var itemInd = 0
+		for item in inputArray:
+			if itemInd % 2:
+				inputArray.remove(itemInd)
+			itemInd+=1
 	return inputArray
+
+
+
+
+
